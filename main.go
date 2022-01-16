a4e1f7ce0832408759e9f068f556e1f2f6706769
koyaaoyama <aoyama.ko0920@gmail.com>
2022-01-16 17:08:43 +0900

    config

--- main.go
+++ main.go
@@ -4,11 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"hex_to_color_name/controllers"
+	"log"
 	"os"
+
+	"gopkg.in/ini.v1"
 )
 
+type ConfigList struct {
+	BaseUrl string
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("config/config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		BaseUrl: cfg.Section("api").Key("base_url").String(),
+	}
+}
+
 func main() {
-	fmt.Println("ENTER HEX CODE:")
+	fmt.Println(`ENTER HEX CODE WITHOUT "#":`)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
